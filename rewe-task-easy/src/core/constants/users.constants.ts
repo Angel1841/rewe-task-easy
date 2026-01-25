064daafb7e1757ded4121b369049629002a11c04
Angel Atanasov <A201807@rewe-digital.com>
2026-01-25 14:27:43 +0200

    user module and base accs added

---
+++ rewe-task-easy/src/core/constants/users.constants.ts
@@ -0,0 +1,6 @@
+import { User } from '../model/user.model';
+
+export const USERS: ReadonlyArray<User> = [
+  { id: 1, username: 'alice', password: '1234' },
+  { id: 2, username: 'bob', password: '1234' }
+];
